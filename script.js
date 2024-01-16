884559c49f6598b778e7f8db0a93d3f8c5d2ee37
dfussell1 <daltonfussell1@gmail.com>
2024-01-16 14:04:47 -0600

    added function for generating password

--- script.js
+++ script.js
@@ -101,6 +101,59 @@ var specChar = [
   '.',
 ];
 
+function generatePassword() {
+
+  var optionsArr = [];
+  var password = '';
+
+  var passLength = parseInt(prompt("Enter Desired Password Length Between 8-128"));
+
+// This ensures the user enters a numerical character
+  if(isNaN(passLength)) {
+    alert('Please enter a number');
+    return;
+  }
+
+// User must enter a number between 8 and 128
+  if(passLength < 8 || passLength > 128) {
+    alert('Please enter a number between 8 and 128');
+    return;
+  } 
+// Asks user what type of characters they would like to include in their password
+  var includeUppercase = confirm('Would you like uppercase letters?');
+  var includeLowercase = confirm('Would you like lowercase letters?');
+  var includeNumbers = confirm('Would you like to include numbers?');
+  var includeSpecChars = confirm('Would you like to include special characters?');
+
+  // Ensures user selects at least one of the prompts above 
+  if(!includeUppercase && !includeLowercase && !includeNumbers && !includeSpecChars) {
+    alert('Please select at least one character type');
+    return;
+  }
+
+  // Creates the new array based on user's selected options
+  if(includeUppercase) {
+    optionsArr = optionsArr.concat(upperCase);
+  }
+  if(includeLowercase) {
+    optionsArr = optionsArr.concat(lowerCase);
+  }
+  if(includeNumbers) {
+    optionsArr = optionsArr.concat(numerical);
+  }
+  if(includeSpecChars) {
+    optionsArr = optionsArr.concat(specChar);
+  }
+
+  for (var i = 0; i < passLength; i++) {
+    var randomIndex = Math.floor(Math.random() * optionsArr.length);
+    password += optionsArr[randomIndex];
+  }
+
+  return password;
+
+}
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
